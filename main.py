7e104f1375329ed10501e8c924c19a33d3d4785c
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2025-07-29 08:52:16 +0100

    * Added a formatter (black) * Added a linter (flake8) * Added a type checker (mypy) * Updated projects toml file * Checked, formatted and corrected code

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
+from constants import (
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    ASTEROID_KINDS,
+    ASTEROID_MAX_RADIUS,
+    ASTEROID_MIN_RADIUS,
+    ASTEROIDSPAWN_RATE,
+)
+
+
+def main() -> None:
+    print("Starting Asteroids!")
 
 
 if __name__ == "__main__":
