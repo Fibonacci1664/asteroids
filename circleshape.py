07af2f63b2f6eaa5c7e12ab7a10b9d9e184fefd2
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2025-08-01 08:24:55 +0100

    * Added a circle shape class * CH2 L4 completed

---
+++ circleshape.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+        
+
+        self.position = pygame.Vector2(x, y)
+        self.position = pygame.Vector2(0, 0)
+    
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
