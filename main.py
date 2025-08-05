966422cf5b80ea13f3305f02182aecedd06691c0
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2025-08-05 08:53:52 +0100

    * Lint and format

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from constants import (
     ASTEROID_MAX_RADIUS,
     ASTEROID_MIN_RADIUS,
     ASTEROID_SPAWN_RATE,
-    PLAYER_RADIUS
+    PLAYER_RADIUS,
 )
 
 
@@ -33,11 +33,11 @@ def main() -> None:
                 return
 
         screen.fill(colour)
-        #print(f"Player position: {player.position}, radius: {PLAYER_RADIUS}")
+        # print(f"Player position: {player.position}, radius: {PLAYER_RADIUS}")
         player.draw(screen)
         pygame.display.flip()
 
-        dt = (clock.tick(60) / 1000)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
